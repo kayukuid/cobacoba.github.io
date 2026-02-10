0fa55e586fb6009e93b69e75945321d2d6d2b535
kayukuid <adisaputra636@gmail.com>
2026-02-10 09:38:13 +0700

    Update Header Mobile

--- js/main.js
+++ js/main.js
@@ -74,3 +74,14 @@ window.onclick = function(e) {
         closeModal();
     }
 };
+<script>
+window.addEventListener("scroll", () => {
+  const header = document.querySelector(".header");
+  if (window.scrollY > 50) {
+    header.classList.add("shrink");
+  } else {
+    header.classList.remove("shrink");
+  }
+});
+</script>
+
